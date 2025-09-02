4ab2e3db69050f5bf1b281c7f9140433378cc203
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-09-03 00:36:30 +0530

    fix(auth): fixed a uri mismatch issue with google oauth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
+    baseURL: 'https://tadoo.adith.me'
 })
 
 export const {useSession, signOut} = authClient;
